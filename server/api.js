51eb2ac67cd7274474620177526f3e4c5bd507f1
Jeremy Kim <jeremy@smartly.io>
2021-09-03 14:45:02 +0200

    Placeholders for minions route

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const minionsRouter = require('./routes/minions');
 const apiRouter = express.Router();
 
-
+apiRouter.use('/minions', minionsRouter);
 
 module.exports = apiRouter;
---
+++ server/routes/minions.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = require('../../server');
+const minionsRouter = express.Router();
+
+// GET array of all minions
+minionsRouter.get('/', (req, res, next) => {
+
+});
+
+// POST create a new minion and save it to the database
+minionsRouter.post('/', (req, res, next) => {
+
+});
+
+// GET a single minion by id
+minionsRouter.get('/:minionId/', (req, res, next) => {
+
+});
+
+// PUT update a single minion by id
+minionsRouter.put('/:minionId/', (req, res, next) => {
+
+});
+
+// DELETE a single minion by id
+minionsRouter.delete('/:minionId/', (req, res, next) => {
+
+});
+
+module.exports = minionsRouter;
