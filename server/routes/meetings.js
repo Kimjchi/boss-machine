c616dd88f558b8268ea3692e6a846962325f54af
Jeremy Kim <jeremy@smartly.io>
2021-09-03 15:01:41 +0200

    Add functions to meetings routes

--- server/routes/meetings.js
+++ server/routes/meetings.js
@@ -1,19 +1,23 @@
 const express = require('express');
+const { createMeeting, getAllFromDatabase, deleteAllFromDatabase, addToDatabase } = require('../db');
 const meetingsRouter = express.Router();
 
 // GET array of all meetings
 meetingsRouter.get('/', (req, res, next) => {
-
+    res.send(getAllFromDatabase('meetings'));
 });
 
 // POST create a new meeting and save it to the database
 meetingsRouter.post('/', (req, res, next) => {
-
+    const newMeeting = createMeeting();
+    addToDatabase('meetings', newMeeting);
+    res.status(201).send(newMeeting);
 });
 
 // DELETE all meetings from the database
 meetingsRouter.delete('/', (req, res, next) => {
-
+    const newModel = deleteAllFromDatabase('meetings');
+    res.status(204).send();
 });
 
 module.exports = meetingsRouter;
